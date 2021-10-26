1baf901bf0f753f90bed43828a9daba8c7263521
Victor Leão <leaovic@gmail.com>
2021-10-25 22:50:29 -0300

    Aula 04 e exercicios

---
+++ Programacao_Imperativa/Aula4.js
@@ -0,0 +1,24 @@
+console.log("Olá Mundo!");
+
+var x = 8;//isso e uma criação de variável
+var y = 5;
+x = 5;//atribuí outro valor a variável
+const z = 34;//tipo constate
+let h = 96;
+h = 78;
+
+let nome = "Victor";//tipo string
+let plano = true;//booleano
+
+let soma = h + x;
+let sub = h - x;
+let div = h / x;
+let mult = h * x;
+
+
+console.log(nome,plano,soma,sub,mult,div);
+
+
+
+
+
---
+++ Programacao_Imperativa/exercicioAula04Sala05.js
@@ -0,0 +1,10 @@
+
+
+let nome = "Ana";
+let idade = 26;
+let peso = 55;
+let altura = 1.62;
+let plano = true;
+let imc = peso / (altura * altura);
+
+console.log(`${nome} tem ${idade} anos e seu IMC é de ${imc}`);//Obrigado Thabata Carrion
